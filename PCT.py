c4e299ed66d3603ba3e1c2072adcb32555850ff9
Phage-Official <100456211+Phage-Official@users.noreply.github.com>
2024-05-22 11:35:30 +0800

    Add files via upload
    optimize sturcture and now it can generate AST

--- PCT.py
+++ PCT.py
@@ -1,100 +1,135 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-!
-#2024/05/20 - 23:37
-
-"""
-        This is a script hot as f*ck for compiling C(more like dialect) source file to MASM Syntax real mode .ASM file
-
-        !=============================！
-        !                             !
-        !   sorry dude I have ADHD    ！
-        !                             !
-        !=============================!
-"""
-
-import sys
 import os
+import sys
 
+def parse(line,idx):
+    global headFiles
+    if line[0:8] == "#include":
+        headFiles = preprocessor(line[8:],idx)
+        print("[Info] Add head File "+headFiles[-1]+' to head file list')
+        return  headFiles
+    else:
+        for types in dataType:
+            if line[0:len(types)] == types:
+                obj = {"DataType":dataType[types]}
+                if types == "int":
+                    obj = parseInt(line[len(types):],obj,idx)
+                    synTree.append(obj)
+                    return obj
 
-#initialization
-
-def genObjFile():
-        for i in range(0,len(src)-1):
-                if src[i] == ".":
-                        asmOutputFile = open(src[:i]+".asm","w")
-                        return asmOutputFile
-        print("[Warning] Source File's suffix is not determined, this may cause random error")
-        
-if len(sys.argv) != 1:
-	for args in sys.argv[1:]:
-		if args == "-h":
-			print("pct86 - A tranlator for real mode C language\n\n[Usage]\n\tpct86 [file]")
-			exit()
-		else:
-			src = args
-			asmOutputFile = genObjFile()
-
-
-try:
-	src = open(src,"r")
-except:
-	print("[Error] File Error Occurs")
-	exit()
-
-#variables
-headerFiles = []
-index = 1
-entryRet  = ""
-
-#const set
-data = {'int':'int','char':'char','double':'double','int*':'int pointer','char*':'char pointer','void':'void','void*':'void pointer'} #sorry float point not support yet
-
-def noAuxillary(line):
-        line = line.replace("\t","")
-        line = line.replace("\r","")
-        line = line.replace("\n","")
-        line = line.replace(" ","")
-        return line
-
-def entry():
-        asmOutputFile.write("assume cs:code\ncode segment\nmain:")
-        if entryType == "void" or entryType == "void pointer":
-                RetVal = ""
-        else:
-                RetVal = "ret"
-                
-        
-
-for line in src:
-
-	#process header file
-        index+=1
-        line = noAuxillary(line)
-        if line[0:8] == "#include":
-                header = line[8:]
-                if header[0] == "<" and header[-1] == ">":
-                        if sys.platform == "win32":
-                                PATH = input("[Hint] Specify Standard Header File Path:")
-                                header = {'Name':header[1:-1],'File':PATH+header[1:-1]}
-                        else:
-                                header = {'Name':header[1:-1],'File':"/usr/include/sys/"+header[1:-1]}
-                elif header[0] == '"' and header[-1] == '"':
-                        header = {'Name':header[1:-1],'File':"./"+header[1:-1]}
-                else:
-                        print("[Error] Invalid format of header file")
-                        exit()
-                headerFiles.append(header)
-                print("[Info] header files:",headerFiles)
-
-        #main process
+def parseInt(line,obj,idx):
+    if line[-1] == "{":
         for i in range(0,len(line)-1):
-                if line[i:i+6] == "main()" and line[i+6] == "{":
-                        try:
-                                data[line[:i]]
-                        except:
-                                print("[Error] Undefined type of Entry Point")
-                                exit()
-                        entryType = data[line[:i]]
-                        print("[Info] Entry Point Type:",entryType)
-                        print("[Proc] Write Entry Point to .ASM source file")
-                        entry()
+            if line[i] == "(":
+                #parse argument
+                obj["arg"] = parse(line[i:-2],idx)
+                obj["val"] = "int func()"
+                obj["var"] = line[0:i]
+
+                return obj
+                    
+    elif line[-1] == ";":
+        for i in range(0,len(line)-1):
+            if line[i] == "=":
+                obj["val"] = line[i+1:-1]
+                obj["var"] = line[0:i]
+        return obj
+
+def parseVoid(line, obj, idx):
+    if line[-1] == "{":
+        for i in range(0, len(line) - 1):
+            if line[i] == "(":
+                obj["arg"] = parse(line[i:-2], idx)
+                obj["val"] = "void func()"
+                obj["var"] = line[0:i]
+                return obj
+    elif line[-1] == ";":
+        for i in range(0, len(line) - 1):
+            if line[i] == "=":
+                obj["val"] = line[i+1:-1]
+                obj["var"] = line[0:i]
+        return obj
+
+def parseChar(line, obj, idx):
+    if line[-1] == "{":
+        for i in range(0, len(line) - 1):
+            if line[i] == "(":
+                obj["arg"] = parse(line[i:-2], idx)
+                obj["val"] = "char func()"
+                obj["var"] = line[0:i]
+                return obj
+    elif line[-1] == ";":
+        for i in range(0, len(line) - 1):
+            if line[i] == "=":
+                obj["val"] = line[i+1:-1]
+                obj["var"] = line[0:i]
+        return obj
+
+def parsePointer(line, obj, idx):
+    if line[-1] == "{":
+        for i in range(0, len(line) - 1):
+            if line[i] == "(":
+                obj["arg"] = parse(line[i:-2], idx)
+                if "int*" in line:
+                    obj["val"] = "int pointer func()"
+                elif "char*" in line:
+                    obj["val"] = "char pointer func()"
+                elif "void*" in line:
+                    obj["val"] = "void pointer func()"
+                obj["var"] = line[0:i]
+                return obj
+    elif line[-1] == ";":
+        for i in range(0, len(line) - 1):
+            if line[i] == "=":
+                obj["val"] = line[i+1:-1]
+                obj["var"] = line[0:i]
+        return obj
+
+def preprocessor(line,idx):
+    global lastHeadIdx
+    if idx - lastHeadIdx >= 2:
+        exit()
+    if line[0] == "<" and line[-1] == ">":
+        headFile = line[1:-1]
+        lastHeadIdx = idx
+    elif line[0] == '"' and line[-1] == '"':
+        headFile = line[1:-1]
+        lastHeadIdx = idx
+    headFiles.append(headFile)
+    return headFiles
+
+if __name__ == "__main__":
+
+    #variable
+    lastHeadIdx = 0
+    headFiles = []
+    synTree = []
+
+    #constant
+    dataType = {"int":"int","char":"char","void":"void","int*":"int ptr","char*":"char ptr","void*":"void ptr"}
+    
+    if len(sys.argv) != 1:
+        args = sys.argv[1:]
+        for arg in args:
+            if arg == "-h":
+                print("\nPCT.py a python script compile C to ASM\n[Usage]\n\tpct.py [file]")
+            else:
+                src = arg
+    else:
+        exit()
+    
+    try:
+        source = []
+        src = open(src,"r")
+        for line in src:
+            line = line.replace("\n","").replace("\t","")
+            line = line.replace("\r","").replace(" ","")
+            source.append(line)
+        src = source
+    except:
+        print("[Error] File Error Occurs")
+        exit()
+
+    for i in range(0,len(src)-1):
+        parse(src[i],i)
+
+    print("syntax:",synTree,"\nheadfiles:",headFiles)
